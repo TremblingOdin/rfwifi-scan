0241d4c9f08cc90f40d024a82d37adba28e25c1f
TremblingOdin <pigmekevin@gmail.com>
2020-08-24 12:56:04 -0600

    initiated python file

---
+++ python/src/rfpiwifi.py
@@ -0,0 +1,6 @@
+import RPi.GPIO as GPIO
+# at the moment of writing this I am working with an mfrc522
+from mfrc522 import SimpleMFRC522
+
+
+# at the moment of writing this I am working with a pi 3 due to it's included wifi capabilities
